30033af7d03be36b4b8cea1689f02b36b32d856c
RAM <sebastien.rsn@gmail.com>
2025-02-27 21:07:08 +0100

    adding a file to handle themes: not coded yet

--- backend/themes.py
+++ backend/themes.py
@@ -1,18 +1,1 @@
-import os
-
-
-def load_stylesheet(theme: str):
-    """Charge le fichier QSS du thème sélectionné."""
-    base_path = os.path.dirname(
-        os.path.abspath(__file__)
-    )  # 📂 Récupère le dossier `stylesheets`
-    file_path = os.path.join(base_path, f"{theme}.qss")
-
-    if not os.path.exists(file_path):
-        print(
-            f"❌ Le fichier {file_path} n'existe pas. Chargement du style par défaut."
-        )
-        return ""
-
-    with open(file_path, "r", encoding="utf-8") as f:
-        return f.read()
+# Logic for selected theme
