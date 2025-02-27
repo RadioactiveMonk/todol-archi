3b76dc5da9d11b98d81576590431f589ebafbfb3
RAM <sebastien.rsn@gmail.com>
2025-02-27 20:34:48 +0100

    p

--- gui/widgets.py
+++ gui/widgets.py
@@ -1,8 +1,10 @@
+from typing import Optional
 from PyQt6.QtWidgets import (
     QPushButton,
     QLineEdit,
     QHeaderView,
     QTableView,
+    QWidget,
 )
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import QDir
@@ -12,7 +14,9 @@ from backend.constants import TASK_HEADERS
 class CustomButton(QPushButton):
     """Bouton personnalisé avec icône et tooltip."""
 
-    def __init__(self, icon_name: str, tooltip: str, parent=None) -> None:
+    def __init__(
+        self, icon_name: str, tooltip: str, parent: Optional[QWidget] = None
+    ) -> None:
         super().__init__(parent)
         icon_path = QDir.current().filePath(f"resources/icons/{icon_name}")
         self.setIcon(QIcon(icon_path))
@@ -22,13 +26,15 @@ class CustomButton(QPushButton):
 class SearchBar(QLineEdit):
     """Barre de recherche"""
 
-    def __init__(self, parent=None) -> None:
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
         super().__init__(parent)
         self.setPlaceholderText(" Search tasks ...")
         self.setFixedHeight(40)
 
 
 class TaskTable(QTableView):
-    """Tableau des tâches avec configuration personnalisée."""
+    """Configuration graphique des tâches. Aucune logique métier, gérée par backend.TaskTableModel"""
 
-    pass
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
+        super().__init__(parent)
+        self.setSortingEnabled(True)
