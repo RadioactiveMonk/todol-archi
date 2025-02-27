b3ef60e31488a64a7bcf815917637b1c536134e5
RAM <sebastien.rsn@gmail.com>
2025-02-27 15:52:40 +0100

    refactoring:task

--- backend/task.py
+++ backend/task.py
@@ -1,29 +1,31 @@
 from dataclasses import dataclass, field
-from datetime import datetime
 import uuid
 from PyQt6.QtCore import QDate
+from typing import Optional
 
 
 @dataclass
 class Task:
     """Représente une tâche dans la To-Do List."""
 
-    status: bool = False
-    category: str = ""
+    status: Optional[bool] = False
+    category: Optional[str] = ""
     title: str = ""
-    notes: str = ""
-    expiration: QDate = field(default_factory=lambda: QDate.currentDate().addDays(1))
+    notes: Optional[str] = ""
+    expiration: Optional[QDate] = field(
+        default_factory=lambda: QDate.currentDate().addDays(1)
+    )
     task_id: str = field(default_factory=lambda: str(uuid.uuid4()), init=False)
 
     def to_dict(self) -> dict:
         """Convertit une instance de Task en dictionnaire pour la sauvegarde JSON."""
         return {
-            "task_id": self.task_id,
             "status": self.status,
             "category": self.category,
             "title": self.title,
             "notes": self.notes,
             "expiration": self.expiration,
+            "task_id": self.task_id,
         }
 
     @classmethod
@@ -31,14 +33,12 @@ class Task:
         """Crée une instance de Task à partir d'un dictionnaire JSON."""
 
         instance = cls(
-            task_id=data.get("task_id"),
-            title=data.get("title", "Task title not found."),
             status=data.get("status", False),
-            notes=data.get("notes"),
-            expiration=data.get("expiration"),
+            category=data.get("category", "DEFAULT_CATEGORY"),
+            title=data.get("title", ""),
+            notes=data.get("notes", ""),
+            expiration=data.get("expiration", ""),
         )
-    
-
         # Mise à jour des champs non initialisés (init=False)
         instance.task_id = data.get("task_id", str(uuid.uuid4()))
 
