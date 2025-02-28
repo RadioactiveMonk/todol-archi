1d5aff189e9181879a3a8b8383ccf7221f826ac3
RAM <sebastien.rsn@gmail.com>
2025-02-28 20:20:49 +0100

    p

--- gui/main_window.py
+++ gui/main_window.py
@@ -9,7 +9,7 @@ from typing import List, Dict, Any, Optional, Union
 from PyQt6.QtGui import QIcon
 from gui.dialogs.add_task_dialog import AddTaskDialog
 from gui.dialogs.edit_parameters_dialog import EditParametersDialog
-from gui.widgets import CustomButton, SearchTasks, TaskTable
+from gui.widgets import CustomButton, SearchTasks, TaskTable, MenuBar
 from backend.task_manager import TaskManager
 from backend.config import MAIN_WINDOW_TITLE, MAIN_WINDOW_GEOMETRY
 
@@ -26,6 +26,7 @@ class MainWindow(QMainWindow):
         )  # Ajout d'une icône personnalisée
 
         self.task_manager = TaskManager()
+        self.setMenuBar(MenuBar(self))
         self.init_ui()  # Initialisation de l'interface
 
     def init_ui(self) -> None:
@@ -40,17 +41,17 @@ class MainWindow(QMainWindow):
         action_layout: QHBoxLayout = QHBoxLayout()
 
         # Barre de recherche personnalisée
-        self.search_tasks_bar = SearchTasks()
+        self.search_tasks_bar = SearchTasks(self)
         # ️🚩 self.search_tasks ......
         action_layout.addWidget(self.search_tasks_bar)
 
         # Boutons personnalisés
-        self.add_task_button = CustomButton("add.png", "Add new task")
+        self.add_task_button = CustomButton("add.png", "Add new task", self)
         self.add_task_button.clicked.connect(self.open_add_task_dialog)
         action_layout.addWidget(self.add_task_button)
 
         self.edit_parameters_button = CustomButton(
-            "app-parameters.png", "Edit parameters"
+            "app-parameters.png", "Edit parameters", self
         )
         self.edit_parameters_button.clicked.connect(self.open_edit_parameters_dialog)
         action_layout.addWidget(self.edit_parameters_button)
@@ -58,7 +59,7 @@ class MainWindow(QMainWindow):
         main_layout.addLayout(action_layout)  # Ajout du layout de recherche et boutons
 
         # Création du tableau des tâches personnalisé
-        self.task_table = TaskTable()
+        self.task_table = TaskTable(self)
         main_layout.addWidget(self.task_table)
 
         self.refresh_task_list()  # ✅ Charge les tâches dès l’ouverture de l’appli
@@ -73,7 +74,8 @@ class MainWindow(QMainWindow):
 
     def open_edit_parameters_dialog(self):
         """Ouvre la boite d'édition des paramètres"""
-        pass
+        dialog = EditParametersDialog(self)
+        dialog.exec()
 
     def refresh_task_list(self):
         """Met à jour l'affichâge des tâches"""
