f1fbecca2e9ae13b7f4b5eb5ea3c93413d0437dc
RAM <sebastien.rsn@gmail.com>
2025-03-01 18:11:20 +0100

    p

--- gui/widgets.py
+++ gui/widgets.py
@@ -10,7 +10,6 @@ from PyQt6.QtWidgets import (
 )
 from PyQt6.QtGui import QIcon, QAction
 from PyQt6.QtCore import QDir
-from backend.constants import TASK_HEADERS
 
 
 class CustomButton(QPushButton):
