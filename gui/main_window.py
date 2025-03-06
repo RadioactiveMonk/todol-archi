853861881f1bb978df02a365adbb83912776cb23
RAM <sebastien.rsn@gmail.com>
2025-03-06 16:12:23 +0100

    adding theme.py

--- gui/main_window.py
+++ gui/main_window.py
@@ -89,5 +89,6 @@ class MainWindow(QMainWindow):
 
     def refresh_task_list(self):
         """Recharge les tâches et met à jour l'affichage du tableau"""
+
         self.task_table.table_model.tasks = self.db.get_tasks()
-        self.task_table.table_model.layoutChanged.emit()  # 🔥 Met à jour l'affichage
+        self.task_table.table_model.layoutChanged.emit()  # Met à jour l'affichage
