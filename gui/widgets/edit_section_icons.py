26b84284870d67e88d1d16f3eaa35e4f61275c47
RAM <sebastien.rsn@gmail.com>
2025-03-07 17:04:36 +0100

    icons

---
+++ gui/widgets/edit_section_icons.py
@@ -0,0 +1,7 @@
+from PyQt6.QtGui import QIcon
+
+
+ICONS = {
+    "delete": QIcon("resources/icons/delete_task.png"),
+    "edit": QIcon("resources/icons/edit_task.png"),
+    "check": QIcon("resources/icons/check_task.png"),}
