8528eec9d842c6305833843e6f77b449cb1a9dc1
RAM <sebastien.rsn@gmail.com>
2025-03-10 19:29:12 +0100

    status toggle need fix for sql registering

--- backend/database_controler.py
+++ backend/database_controler.py
@@ -11,7 +11,7 @@ class DatabaseControler:
     _queries = {
         "create_table": """CREATE TABLE IF NOT EXISTS tasks (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        status INTEGER DEFAULT 0,
+                        status INTEGER,
                         category TEXT,
                         expiration TEXT,
                         title TEXT NOT NULL,
