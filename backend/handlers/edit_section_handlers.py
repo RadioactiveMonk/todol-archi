f6c364166a6b748ee15c6b5b71fc378f46b285c4
doyoudance <sebastien.rsn@gmail.com>
2025-03-21 22:49:22 +0100

    handlers optimisation

--- backend/handlers/edit_section_handlers.py
+++ backend/handlers/edit_section_handlers.py
@@ -1,5 +1,7 @@
+from dataclasses import asdict
 from backend.database.db_manager import DbManager
 from backend.core.logger import logger
+from backend.models.task import Task
 
 
 class TaskHandlers:
@@ -13,20 +15,33 @@ class TaskHandlers:
 
         result = self.db.delete_task(task_id)
         if result:
-            logger.info(f"EDIT SECTION: task deleted successfully -- {task_id}")
+            logger.info(f"🗑 Task deleted successfully (id={task_id})")
         else:
-            logger.warning(f"EDIT SECTION: task couldn't be deleted -- {task_id}")
+            logger.warning(f"❌ Failed to delete task (id={task_id})")
         return result
 
-    def edit_handler(self, task_id: int, **kwargs) -> bool:
+    def edit_handler(self, task: Task) -> bool:
         """Updates a task in the DB"""
-        if not kwargs:
-            logger.info(f"EDIT SECTION: Nofields provided to update task {task_id}")
 
-        result = self.db.update_task(task_id, **kwargs)
+        if task.tid is None:
+            logger.warning(f"✏️ Cannot edit task: missing ID")
+            return False
+
+        data = asdict(task)
+        task_id = data.pop("tid")  # ID à part
+        filtered_data = {k: v for k,v in data.items() if v is not None}
+
+        if not filtered_data:
+            logger.warning(f"✏️ No fields provided for editing task (id={task_id})")
+            return False
+        
+        result = self.db.update_task(task_id, **filtered_data)
 
         if result:
-            logger.info(f"EDIT SECTION: task {task_id} updated successfully -- Changes: {kwargs}")
-        else: logger.warning(f"EDIT SECTION: task {task_id} couldn't be updated -- No changes applied")
+            logger.info(f"✅ Task updated (id={task_id}) -- Changes: {filtered_data}")
+        else:
+            logger.warning(
+                f"⚠️ Task update failed (id={task_id}) -- No changes detected"
+            )
 
         return result
