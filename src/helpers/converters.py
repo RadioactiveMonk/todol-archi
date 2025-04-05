8bb91ac55a5ec8900d98caf2a12057f655c18d1e
doyoudance <sebastien.rsn@gmail.com>
2025-04-05 15:32:01 +0200

    dataclasstodict type ok

--- src/helpers/converters.py
+++ src/helpers/converters.py
@@ -4,7 +4,7 @@ from typing import Any, Dict
 
 def dataclass_to_dict(obj: Any, exclude: set[str] | None = None) -> Dict[str, Any]:
     """
-    Convertit une dataclass en dictionnaire, avec possibilité d'exclure certains champs.
+    Convertit une dataclass (obj) en dictionnaire, avec possibilité d'exclure certains champs.
 
     Args:
         obj: Instance d'une dataclass.
@@ -13,8 +13,10 @@ def dataclass_to_dict(obj: Any, exclude: set[str] | None = None) -> Dict[str, An
     Returns:
         Un dictionnaire représentant la dataclass.
     """
-    if not is_dataclass(obj):
-        raise TypeError("L'objet fourni n'est pas une dataclass")
+    if not is_dataclass(obj) or isinstance(obj, type):
+        raise TypeError(
+            "L'objet fourni doit être une instance de dataclass, pas une classe."
+        )
 
     exclude = exclude or set()
     return {k: v for k, v in asdict(obj).items() if k not in exclude}
