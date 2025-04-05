5259077b0d90681255f40a71e3508ebca5e1cdda
doyouDance <sebastien.rsn@gmail.com>
2025-04-06 01:28:17 +0200

    Update reload_all.py

--- scripts/reload_all.py
+++ scripts/reload_all.py
@@ -12,16 +12,16 @@ from core.path import DB_FILE
 from models import task
 
 # Rechargement du module task et instanciation
-""" reload(task)
-Task = task.Task """
+reload(task)
+Task = task.Task
 
-""" test_task = Task(
+test_task = Task(
     completed=True,
     category="Work",
     expiration="2025-09-07 21:00",
     title="Test task",
     notes="Test notes",
-) """
+)
 
 # Initialisation DB
 init_db()
@@ -34,33 +34,6 @@ db = AskDB(conn)
 # Interface
 print("✅ DB session")
 print("📌 help(db) for commands.")
-
-# ================================
-# Commandes pratiques IPython
-# ================================
-
-
-# Ajouter une tâche de test rapidement (via ask)
-def insert_test_task():
-    """Ajoute une tâche générique pour test rapide."""
-    db.ask(
-        "insert",
-        SQL_INSERT_TASK,
-        "Test IPython",
-        "debug",
-        0,
-        "2025-05-01",
-        "Créée via insert_test_task()",
-    )
-    print("✅ Tâche de test insérée.")
-
-
-# Afficher toutes les tâches
-def show_tasks():
-    """Affiche toutes les tâches dans la table."""
-    for row in db.get_all_tasks():
-        print(dict(row))
-
 
 # Supprimer toutes les tâches (⚠️)
 def clear_tasks():
