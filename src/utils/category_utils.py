9a3d97427e97ffd01f7e94ecd0570aa58a88e6ec
doyoudance <sebastien.rsn@gmail.com>
2025-04-12 20:52:59 +0200

    small tweaks

--- src/utils/category_utils.py
+++ src/utils/category_utils.py
@@ -2,12 +2,14 @@ from functools import lru_cache
 from typing import List
 
 from helpers.contextmanagers import open_settings
+from utils.log_utils import logger
 
 
 @lru_cache
 def get_categories() -> List[str]:
     """Return the list of categories from the settings file (cached)"""
     
+    logger.debug("Accesing task categories")
     with open_settings() as settings:
         categories = settings.get("categories", [])
         return categories if isinstance(categories, list) else []
