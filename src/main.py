a4c161e71ac2de8b26a26461fdb1204ee4002f7a
doyoudance <sebastien.rsn@gmail.com>
2025-04-28 20:34:06 +0200

    see todo/ or MIGRATION_log.md

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import sys
 from PySide6.QtWidgets import QApplication
 from rich.traceback import install
 
-from core.factory.mainwindow_factory import MainWindowFactory
+from core.factories.mainwindow_factory import MainWindowFactory
 from utils.init_db import init_db
 
 
